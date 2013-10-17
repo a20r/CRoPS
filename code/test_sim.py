345486769ec17fdbfc0d0227a2056060b93b9614
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-10-17 21:23:37 +0100

    Made it so it isnt creating stupid files

--- code/test_sim.py
+++ code/test_sim.py
@@ -1,4 +1,4 @@
-import boidsimulation as bs 
+import boidsimulation as bs
 import sys
 
 """
@@ -8,21 +8,21 @@ Main module to run for testing purposes
 if __name__ == "__main__":
     mapDict = {
     	"maps/scene2.map": {
-    		"startPoint": (494, 213), 
+    		"startPoint": (494, 213),
     		"endPoint": (404, 20)
-    	}, 
+    	},
     	"maps/scene3.map": {
-    		"startPoint": (356, 42), 
+    		"startPoint": (356, 42),
     		"endPoint": (852, 450)
-    	}, 
+    	},
     	"maps/scene1.map": {
-    		"startPoint": (50, 50), #(50, 600) 
+    		"startPoint": (50, 50), #(50, 600)
     		"endPoint": (980, 30)
     	}
     }
     fs = bs.FlockSim(
-    	int(sys.argv[2]), 
-    	mapDict[sys.argv[1]]["startPoint"], 
+    	int(sys.argv[2]),
+    	mapDict[sys.argv[1]]["startPoint"],
     	mapDict[sys.argv[1]]["endPoint"]
     )
-    fs.animate()
+    fs.render()
