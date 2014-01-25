92382f9bd79bf864372cfb26a473354cdfe453b0
Chris Choi <chutsu@gmail.com>
2014-01-25 16:56:17 +0000

    Move CodePolygons out of code dir into tools

---
+++ tools/CodePolygons/CMakeFiles/Abetare.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/External/ShewchukTriangle.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/External/ShewchukTriangle.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Programs/Abetare.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Programs/Abetare.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Programs/GEditor2D.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Programs/GEditor2D.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Programs/GenerateScenes.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Programs/GenerateScenes.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/Algebra3D.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/Algebra3D.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/DLHandler.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/DLHandler.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/DisjointSet.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/DisjointSet.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GCamera.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GCamera.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GDraw.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GDraw.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GManager.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GManager.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GText3D.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GText3D.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GTexture.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GTexture.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/GenerateMaze2D.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/GenerateMaze2D.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/Geometry.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/Geometry.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/Grid.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/Grid.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/Polygon2D.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/Polygon2D.o"
+  "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/src/Utils/PseudoRandom.cpp" "/Users/chutsu/Dropbox/proj/toys/CRoPS/tools/CodePolygons/CMakeFiles/Abetare.dir/src/Utils/PseudoRandom.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_USE_MATH_DEFINES"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/OpenGL.framework"
+  "/System/Library/Frameworks/GLUT.framework/Headers"
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
