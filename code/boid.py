18b6379619ea9c3076c9d4f83b358e3ab685aaf8
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-01-30 17:41:50 +0000

    @chutsu RUN THE EXPERIMENTS

--- code/boid.py
+++ code/boid.py
@@ -653,7 +653,7 @@ class Boid:
                 if self.norm(c_point, self.position) < min_obstacle_dist:
                     obstacles_too_close = True
 
-            if not obstacles_too_close or self.stuck:
+            if self.stuck or not obstacles_too_close:
                 gVector, gMagSum = self.getGoalVector()
 
             neighborVectorList, nIndexes = self.getNeighborVectorList()
